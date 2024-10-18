539e8ad19feefe103e3b81a22ea3b7581655da8d
daniel-kitzmann <daniel.kitzmann@gmail.com>
2024-10-18 14:14:55 +0200

    Small changes to chemistry module

---
+++ src/chemistry/step_function_chemistry.h
@@ -0,0 +1,52 @@
+/*
+* This file is part of the BeAR code (https://github.com/newstrangeworlds/BeAR).
+* Copyright (C) 2024 Daniel Kitzmann
+*
+* BeAR is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* BeAR is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You find a copy of the GNU General Public License in the main
+* BeAR directory under <LICENSE>. If not, see
+* <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _step_function_chemistry_h
+#define _step_function_chemistry_h
+
+#include "chemistry.h"
+
+
+#include <vector>
+#include <string>
+
+
+namespace bear {
+
+
+
+class StepFunctionChemistry : public Chemistry{
+  public:
+    StepFunctionChemistry(
+      const std::string& chemical_species);
+    virtual ~StepFunctionChemistry() {}
+
+    virtual bool calcChemicalComposition(
+      const std::vector<double>& parameters,
+      const std::vector<double>& temperature,
+      const std::vector<double>& pressure,
+      std::vector<std::vector<double>>& number_densities,
+      std::vector<double>& mean_molecular_weight);
+  private:
+};
+
+
+}
+#endif 
